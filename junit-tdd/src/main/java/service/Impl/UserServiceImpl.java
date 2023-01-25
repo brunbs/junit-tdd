ec9ae7fe870aad6c1a289c9142ae69e010463177
brunbs <babosa227@gmail.com>
2023-01-25 14:53:01 -0300

    Create UsersRepository

--- junit-tdd/src/main/java/service/Impl/UserServiceImpl.java
+++ junit-tdd/src/main/java/service/Impl/UserServiceImpl.java
@@ -1,11 +1,21 @@
 package service.Impl;
 
+import data.UsersRepository;
+import data.UsersRepositoryImpl;
+import exception.UserServiceException;
 import model.User;
 import service.UserService;
 
 import java.util.UUID;
 
 public class UserServiceImpl implements UserService {
+
+    UsersRepository usersRepository;
+
+    public UserServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
     @Override
     public User createUser(String firstName, String lastName, String email, String password, String repeatPassword) {
 
@@ -13,6 +23,12 @@ public class UserServiceImpl implements UserService {
             throw new IllegalArgumentException("User's first name is empty");
         }
 
-        return new User(firstName, lastName, email, UUID.randomUUID().toString());
+        User user = new User(firstName, lastName, email, UUID.randomUUID().toString());
+
+        boolean isUserCreated = usersRepository.save(user);
+
+        if(!isUserCreated) throw new UserServiceException("Could not create user");
+
+        return user;
     }
 }
